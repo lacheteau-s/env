2a17dc180b8b15f8c57a6a86500922ab231fa2eb
Sulli <lacheteau.s@gmail.com>
2021-01-14 17:13:32 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt upgrade -y
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# TODO: git config
