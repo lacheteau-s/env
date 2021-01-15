d1488fd790ce1abc22bdf3ec265ced54b5a272f6
Sulli <lacheteau.s@gmail.com>
2021-01-15 15:15:56 +0100

    ZSH config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
+echo "UPDATING..."
 sudo apt-get update && sudo apt upgrade -y
+
+echo "INSTALLING ZSH..."
 sudo apt install zsh
+
+echo "INSTALLING OH-MY-ZSH"
 sudo apt-get install powerline fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp zsh-theme $ZSH/themes/$USER.zsh-theme
+
+echo "COPYING ZSH CONFIG..."
+cp zsh-theme $ZSH/themes/sulli.zsh-theme
+mv ~/.zshrc ~/.zshrc.default
+cp .zshrc ~/.zshrc
+source ~/.zshrc
 
 # TODO: git config
